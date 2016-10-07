bf1a0c6221d81cb7271d80aed3c7add2763ead5d
arisros <arisjiratkurniawan@gmail.com>
2016-10-08 01:00:41 +0700

    convert john papa style guide

--- js/app.js
+++ js/app.js
@@ -1,115 +1,138 @@
 // Modul sortApp
-angular.module('sortApp', []);
+(function(angular) {
+  'use strict';
+  angular.module('sortApp', []);
+})(angular);
 
 // Controller mainController -> sortApp
-angular
-  .module('sortApp')
-  .controller('MainController', MainController);
-MainController.inject = ['$scope', 'SortAppFactory'];
-function MainController($scope, SortAppFactory) {
-  var vm = this;
+(function(angular) {
+  'use strict';
+  angular
+    .module('sortApp')
+    .controller('MainController', MainController);
+  MainController.inject = ['$scope', 'SortAppFactory'];
+  function MainController($scope, SortAppFactory) {
+    var vm = this;
 
-  vm.sortType     = SortAppFactory.data.sortType; // set the default sort type
-  vm.sortReverse  = SortAppFactory.data.sortReverse;  // set the default sort order
-  vm.searchFish   = SortAppFactory.data.searchFish;     // set the default search/filter term
-  vm.filterCount  = SortAppFactory.data.filterCount;
-  vm.searchName   = SortAppFactory.data;
-  console.log(vm.searchName);
-    // $scope.$watchCollection(function() {
-    //   return SortAppFactory;
-    // }, 
-    // function(oldVal, newVal) {
-    //   console.log(newVal);
-    // });
-  vm.sushi = [
-    { name: 'Cali Roll', fish: 'Crab', tastiness: 2 },
-    { name: 'Philly', fish: 'Tuna', tastiness: 4 },
-    { name: 'Tiger', fish: 'Eel', tastiness: 7 },
-    { name: 'Rainbow', fish: 'Variety', tastiness: 6 },
-    { name: 'Smooch', fish: 'Lungs chicken', tastiness: 3 },
-    { name: 'Cracko', fish: 'Bone duck', tastiness: 6 },
-    { name: 'The Bang', fish: 'Pork Chest', tastiness: 6 }
-  ];
-}
+    vm.sortType     = SortAppFactory.data.sortType; // set the default sort type
+    vm.sortReverse  = SortAppFactory.data.sortReverse;  // set the default sort order
+    vm.searchFish   = SortAppFactory.data.searchFish;     // set the default search/filter term
+    vm.filterCount  = SortAppFactory.data.filterCount;
+    vm.searchName   = SortAppFactory.data.searchName;
+    // console.log(vm.searchName);
+
+    vm.sushi = [
+      { name: 'Cali Roll', fish: 'Crab', tastiness: 2 },
+      { name: 'Philly', fish: 'Tuna', tastiness: 4 },
+      { name: 'Tiger', fish: 'Eel', tastiness: 7 },
+      { name: 'Rainbow', fish: 'Variety', tastiness: 6 },
+      { name: 'Smooch', fish: 'Lungs chicken', tastiness: 3 },
+      { name: 'Cracko', fish: 'Bone duck', tastiness: 6 },
+      { name: 'The Bang', fish: 'Pork Chest', tastiness: 6 }
+    ];
+  }
+})(angular);
 
 // Controller ListRpeaterController -> sortApp
-angular
-  .module('sortApp')
-  .controller('ListRepeaterController', ListRepeaterController);
-ListRepeaterController.$inject = ['$scope','SortAppFactory'];
-function ListRepeaterController($scope, SortAppFactory) {
-  var vm = this;
-}
+(function(angular) {
+  'use strict';
+  angular
+    .module('sortApp')
+    .controller('ListRepeaterController', ListRepeaterController);
+  ListRepeaterController.$inject = ['$scope','SortAppFactory'];
+  function ListRepeaterController($scope, SortAppFactory) {
+    var vm = this;
+    vm.searchName = SortAppFactory.data.searchName;
+  }
+})(angular);
 
-angular
-	.module('sortApp')
-	.directive('listRepeater', listRepeater);
-listRepeater.$inject = ['SortAppFactory'];
-function listRepeater(SortAppFactory) {
-	return {
-		restrict: 'E',
-		replace: true,
-    controller: 'ListRepeaterController',
-    controllerAs: 'vv',
-    templateUrl: 'template-repeat.html',
-    scope: {
-      data : '='
-    },
-    bindToController: true
-  };            
-}
+(function(angular) {
+  'use strict';
+  angular
+    .module('sortApp')
+    .directive('listRepeater', listRepeater);
+  listRepeater.$inject = ['SortAppFactory'];
+  function listRepeater(SortAppFactory) {
+    return {
+      restrict: 'E',
+      replace: true,
+      controller: 'ListRepeaterController',
+      controllerAs: 'lp',
+      templateUrl: 'template-repeat.html',
+      scope: {
+        data : '='
+      },
+      bindToController: true
+    };            
+  }
+})(angular);
+
 // Directive headerFilter -> sortApp
-angular
-  .module('sortApp')
-  .directive('headerFilter', headerFilter);
+(function(angular) {
+  'use strict';
+  angular
+    .module('sortApp')
+    .directive('headerFilter', headerFilter);
 
-headerFilter.$inject = ['SortAppFactory'];
+  headerFilter.$inject = ['SortAppFactory'];
 
-function headerFilter(SortAppFactory) {
-  return {
-    restrict : 'E',
-    replace: true,
-    controller: function ($scope, SortAppFactory) {
-      var vm = this;
-      vm.searchName = SortAppFactory.data.searchName;
-      // SortAppFactory.data.searchName = $scope.$eval(vm.searchName);
-      $scope.$watch(function () {
-        return vm.searchName;
-      }, 
-      function(newVal, oldVal) {
-        SortAppFactory.data.searchName = newVal ;
-        console.log(SortAppFactory.data.searchName);
-      });
-    },
-    controllerAs: 'hf',
-    bindToController: true,
-    scope: {
-      data: '='
-    },
-    templateUrl: 'template-header.html'
-  };
-}
+  function headerFilter(SortAppFactory) {
+    return {
+      restrict : 'E',
+      replace: true,
+      controller: 'HeaderFilterController',
+      controllerAs: 'hf',
+      bindToController: true,
+      scope: {
+        data: '='
+      },
+      templateUrl: 'template-header.html'
+    };
+  }
+})(angular);
+
+(function(angular) {
+  'use strict';
+  angular
+    .module('sortApp')
+    .controller('HeaderFilterController', HeaderFilterController);
+  HeaderFilterController.$inject = ['$scope', 'SortAppFactory'];
+  function HeaderFilterController ($scope, SortAppFactory) {
+    var vm = this;
+    vm.searchName = SortAppFactory.data.searchName;
+    $scope.$watch(function () {
+      return vm.searchName;
+    }, 
+    function(newVal, oldVal) {
+      SortAppFactory.data.searchName = newVal ;
+      console.log(vm.searchName);
+      console.log(SortAppFactory.data.searchName);
+    });
+  }
+})(angular);
 
 // Factory sortApp
-angular
-  .module('sortApp')
-  .factory('SortAppFactory', SortAppFactory);
-SortAppFactory.$inject = [];
-function SortAppFactory() {
-  var service = {   
-    data  : {
-      searchName   : 'Aris',
-      sortType     : 'name', 
-      sortReverse  : false,  
-      searchFish   : '',
-      filterCount  : 0,
-    }
-  };
+(function(angular) {
+  'use strict';
+  angular
+    .module('sortApp')
+    .factory('SortAppFactory', SortAppFactory);
+  SortAppFactory.$inject = [];
+  function SortAppFactory() {
+    var service = {   
+      data  : {
+        searchName   : '',
+        sortType     : 'name', 
+        sortReverse  : false,  
+        searchFish   : '',
+        filterCount  : 0,
+      }
+    };
 
-  return service;
-} 
+    return service;
+  } 
 
-
+})(angular);
 
 
 
